c28a9ca33f4740d7ef5e410d292ba9ba0c9667ab
Parker <parkerproject@gmail.com>
2016-12-28 23:37:13 -0500

    added header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 
+import style from './style.scss';
+
 const Header = () => (
   <header className="site-header">
     <div className="container-fluid">
-
       <span className="site-logo"><strong>TRECK</strong></span>
       <button className="hamburger hamburger--htla">
         <span>toggle menu</span>
       </button>
+
+      <div className="site-header-shown">
+        <div className="dropdown user-menu">
+          <button
+            className="dropdown-toggle"
+            id="dd-user-menu"
+            type="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <img
+              src="img/avatar-2-64.png"
+              alt=""
+            />
+          </button>
+          <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
+            <a className="dropdown-item" href="#profile">
+              <span className="font-icon glyphicon glyphicon-user" />Profile
+            </a>
+            <a className="dropdown-item" href="#settings">
+              <span className="font-icon glyphicon glyphicon-cog" />Settings
+            </a>
+            <a className="dropdown-item" href="#help">
+              <span className="font-icon glyphicon glyphicon-question-sign" />Help
+            </a>
+            <div className="dropdown-divider" />
+            <a className="dropdown-item" href="#log">
+              <span className="font-icon glyphicon glyphicon-log-out" />Logout
+            </a>
+          </div>
+        </div>
+      </div>
+
     </div>
   </header>
 );
--- app/components/Sidebar/index.js
+++ app/components/Sidebar/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router';
 import style from './style.scss';
 
 const Sidebar = () => (
   <nav className={`side-menu ${style.sidebar} `} tabIndex="0">
     <ul className="side-menu-list">
       <li className="pink-red">
-        <a href="activity.html">
+        <Link to="/dashboard">
           <i className="font-icon font-icon-dashboard" />
           <span className="lbl">Dashboard</span>
-        </a>
+        </Link>
+      </li>
+      <li className="blue">
+        <Link to="/feed">
+          <i className="font-icon font-icon-zigzag" />
+          <span className="lbl">My Feed</span>
+        </Link>
+      </li>
+      <li className="gold">
+        <Link to="/tags">
+          <i className="font-icon font-icon-picture-2" />
+          <span className="lbl">Images</span>
+        </Link>
+      </li>
+      <li className="green">
+        <Link to="/tags">
+          <i className="fa fa-tags" />
+          <span className="lbl">Tags</span>
+        </Link>
+      </li>
+      <li className="gray">
+        <Link to="/settings">
+          <i className="fa fa-gear" />
+          <span className="lbl">Settings</span>
+        </Link>
       </li>
     </ul>
   </nav>
